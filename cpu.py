992f6a23a439d119840958459eee1bbed07aef65
segf00lt <joaodear2007@gmail.com>
2022-04-21 15:43:28 -0300

    wrote fetch, decode and execute

--- cpu.py
+++ cpu.py
@@ -1,146 +1,158 @@
 #!/usr/bin/env python3
 
-from os import argv
+from sys import argv, stderr
 from enum import Enum
 
 class Op(Enum):
-    NOP     = 0b000
+    SYS     = 0b000
+    ALUR	= 0b001 # register-register ALU operation
+    ALUI	= 0b010 # register-immediate ALU operation
+    JUMP	= 0b011
+    BRANCH	= 0b100
+    MEM     = 0b101
 
-    OP		= 0b001
-    IMM		= 0b010
 
-    JUMP	= 0b011 # takes constant value
-    JUMPR	= 0b100 # reads value from register
-    BRANCH	= 0b101
+class Fn(Enum):
+    # SYS
+    NOP         = 0b000
+    HALT        = 0b001
+    IN          = 0b010
+    OUT         = 0b011
 
-    LOAD	= 0b110
-    STORE	= 0b111
+    # ALUR and ALUI
+    ADD         = 0b000
+    SUB         = 0b001
+    AND         = 0b010
+    OR          = 0b011
+    XOR         = 0b100
+    LSHIFT      = 0b101
+    MUL         = 0b110
+    DIV         = 0b111
 
-class Funct(Enum):
-    # OP and IMM
-    ADD		= 0b000
-    SUB		= 0b001
-    AND		= 0b010
-    OR		= 0b011
-    XOR		= 0b100
-    LSHIFT  = 0b101
-    MUL     = 0b110
-    DIV     = 0b111
+    # JUMP
+    REL         = 0b000
+    ABS         = 0b001
 
     # BRANCH
-    EQ		= 0b000
-    NE		= 0b001
-    LT		= 0b010
-    GTE	    = 0b011
+    EQ          = 0b000
+    NE          = 0b001
+    LT          = 0b010
+    GTE         = 0b011
 
-    # LOAD and STORE
-    BYTE    = 0b000
-    HWORD   = 0b001
-    WORD    = 0b010
-
-class Mask(Enum):
-    OPCODE  = 0b111
-    FUNCT   = 0b111
-    REG     = 0b11111
-    OFF_24  = 0b111111111111111111111111
-    OFF_19  = 0b1111111111111111111
-    OFF_7  = 0b1111111
-    OFF_18  = 0b111111111111111111
-
+    # MEM
+    LOAD        = 0b000
+    STORE       = 0b001
 
 # 33 registers:
 # 1 zero register, 31 general purpose and a program counter
-regnames = ['x0'] + [f'x{i}' for i in range(1, 32)] + ['PC']
-PC = 32
+regnames = ['r0'] + [f'r{i}' for i in range(1, 32)] + ['pc']
+pc = 32
+word = 4 # bytes
 regfile = None
 memory = None
 
 def reset():
     global regfile, memory
     regfile = [0] * 33
-    memory = [0] * 8000
+    memory = [0] * 1000 # memory is word addressable
 
-def alu(funct, x, y):
-    if funct == Funct.ADD:
+def ctl(fn, x):
+    if fn == Fn.HALT:
+        return False
+    elif fn == Fn.NOP:
+        regfile[PC] += word
+    else:
+        io(fn, x)
+    return True
+
+def io(fn, x):
+    if fn == Fn.IN:
+        try:
+            regfile[x] = int(input('<weekcore> '))
+        except ValueError:
+            print('error: non-integer input', file=sys.stderr)
+            exit(1)
+    elif fn == Fn.OUT:
+        print(int(regfile[x]))
+
+def alu(fn, x, y):
+    if fn == Fn.ADD:
         return x + y
-    elif funct == Funct.SUB:
+    elif fn == Fn.SUB:
         return x - y
-    elif funct == Funct.AND:
+    elif fn == Fn.AND:
         return x & y
-    elif funct == Funct.OR:
+    elif fn == Fn.OR:
         return x | y
-    elif funct == Funct.XOR:
+    elif fn == Fn.XOR:
         return x ^ y
-    elif funct == Funct.LSHIFT:
+    elif fn == Fn.LSHIFT:
         return x << y
-    elif funct == Funct.MUL:
+    elif fn == Fn.MUL:
         return x * y
-    elif funct == Funct.DIV:
+    elif fn == Fn.DIV:
         return x / y
 
-def cond(funct, x, y):
+def cond(fn, x, y):
     ret = False
-    if funct == Funct.EQ:
+    if fn == Fn.EQ:
         ret = x == y
-    elif funct == Funct.NE:
+    elif fn == Fn.NE:
         ret = x != y
-    elif funct == Funct.LT:
+    elif fn == Fn.LT:
         ret = x < y
-    elif funct == Funct.GTE:
+    elif fn == Fn.GTE:
         ret = x >= y
     return ret
 
-def decode(inst, offset, mask):
-    return inst & (mask << offset)
+def mem(fn, x, y):
+    if fn == Fn.LOAD:
+        regfile[x] = memory[regfile[y]]
+    elif fn == Fn.STORE:
+        memory[regfile[x]] = regfile[y]
 
-### INSTRUCTION LAYOUTS ###
-#
-# inst layout for int reg-reg operations (OP)
-# 0      2  3      5  6       10  11      12  13      17  18                    31
-# [opcode]  [funct ]  [dest    ]  [reg1    ]  [reg2    ]  [empty                 ]
-#
-# inst layout for int reg-imm operations (IMM)
-# 0      2  3      5  6       10  11      12  13                                31
-# [opcode]  [funct ]  [dest    ]  [reg1    ]  [imm       (const int val)         ]
-#
-# inst layout for imm-uncond-ctrl transfer operations (JUMP)
-# 0      2  3       7  8                                                        31
-# [opcode]  [dest   ]  [offset (24 bits)                                         ]
-#
-# inst layout for uncond-ctrl transfer operations (JUMPR)
-# 0      2  3       7  8      12  13                                            31
-# [opcode]  [dest   ]  [base   ]  [offset (19 bits)                              ]
-#
-# inst layout for cond-ctrl transfer operations (BRANCH)
-# 0      2  3      5  8      12  13    17  18               24  25              31
-# [opcode]  [funct ]  [reg1   ]  [reg2  ]  [offset (7 bits)  ]  [offset (7 bits) ]
-#
-# inst layout for memory access operations (LOAD, STORE)
-# 0      2  3      5  6      10  9      13  14                                  31
-# [opcode]  [funct ]  [reg    ]  [base   ]  [offset (18 bits)                    ]
+def field(i, s, e): # get field of inst between s and e
+    return (i >> e) & ((1 << (s - e + 1)) - 1)
+
+def sext(x, l): # sign extend
+    if x >> (l - 1) == 1:
+        return -((1 << l) - x)
+    else:
+        return x
 
 def step():
     # fetch
-    inst = regfile[PC]
+    inst = memory[regfile[PC]]
+    stat = True
 
     # decode
-    opcode = decode(inst, 0, Mask.OPCODE)
-    funct = decode(inst, 3, Mask.FUNCT)
-
-    
-    if opcode == Op.NOP:
-        # skip to next instruction
-    elif opcode == Op.OP:
-    elif opcode == Op.IMM:
-    elif opcode == Op.JUMP:
-    elif opcode == Op.JUMPR:
-    elif opcode == Op.BRANCH:
-    elif opcode == Op.LOAD:
-    elif opcode == Op.STORE:
+    op = field(inst, 31, 29)
+    fn = field(inst, 28, 24)
+    r_a = field(inst, 23, 19)
+    r_b = field(inst, 18, 14)
+    r_c = field(inst, 13, 9)
+    imm_s = sext(field(inst, 18, 0))
+    imm_i = sext(field(inst, 13, 0))
+    imm_b1 = sext(field(inst, 13, 7))
+    imm_b2 = sext(field(inst, 6, 0))
 
     # execute
+    if op == Op.SYS:
+        stat = ctl(fn, r_a)
+    elif op == Op.ALUR:
+        regfile[r_a] = alu(fn, regfile[r_b], regfile[r_c])
+    elif op == Op.ALUI:
+        regfile[r_a] = alu(fn, regfile[r_b], imm_i)
+    elif op == Op.JUMP:
+        regfile[r_a] = regfile[pc]
+        regfile[pc] = regfile[r_b] if fn == Fn.ABS else (regfile[pc] + imm_i * 4)
+    elif op == Op.BRANCH:
+        regfile[pc] += 4 * (imm_b1 if cond(fn, regfile[r_a], regfile[r_b]) else imm_b2)
+    elif op == Op.MEM:
+        mem(fn, r_a, r_b)
 
     # write
+    # not really a stage yet, will be done after pipeline refactor
 
 
 
